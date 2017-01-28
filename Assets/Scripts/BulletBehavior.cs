746afee00160ac6c33f97b2cea24704eb9f2cb0e
Nicholas Suski <n33kos@gmail.com>
2017-01-27 22:26:03 -0700

    Added Player and bullet prefabs and scripts

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour {
+
+	public float speed = 25;
+
+	void Start () {
+		Destroy(gameObject, 20);
+	}
+	
+	void FixedUpdate () {
+		transform.position += transform.up*(speed/100);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		//Destroy(gameObject);
+	}
+
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	public Rigidbody2D rigidBody;
+	public bool canMove = true;
+	public GameObject bullet;
+
+	void Start () {		
+		rigidBody = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+		if ( canMove ){
+			rigidBody.AddRelativeForce( new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) );
+		}
+
+		if (Input.GetKeyDown("space")){
+			ShootBullet();
+		}
+	}
+
+	void ShootBullet () {
+		Instantiate(bullet, new Vector3(transform.position.x, transform.position.y+(transform.up.y*(transform.localScale.y/2)) ,transform.position.z), transform.rotation );
+	}
+
+}
