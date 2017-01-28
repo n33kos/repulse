3dfc612ab26cdfe84ace37bac2cfe43a5a58876b
Nicholas Suski <n33kos@gmail.com>
2017-01-27 20:09:28 -0700

    Set up basic gui framework and gamestate class

---
+++ Assets/Scripts/GUIHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIHandler : MonoBehaviour {
+	
+	//The gamemode as n integer
+	public int mode = 0;
+
+	//A list of canvases which correspond with different gamemodes.
+	public List<Canvas> canvases = new List<Canvas>();
+
+	void OnGUI () {
+		switch(mode){
+			case 0:
+				drawTitleScreen();
+				break;
+			case 1:
+				drawPlayScreen();
+				break;
+			case 2:
+				drawScoreScreen();
+				break;
+			default:
+				//do nothing
+				break;
+		}
+	}
+
+	public void SetGuiMode (int newMode) {
+		mode = newMode;
+		for(int i = 0; i < canvases.Count; i++){
+			canvases[i].enabled = (i == mode);
+		}
+	}
+
+	void drawTitleScreen () {
+		//Debug.Log("Title Screen");
+	}
+
+	void drawPlayScreen () {
+		//Debug.Log("Play Screen");
+	}
+
+	void drawScoreScreen () {
+		//Debug.Log("Score Screen");
+	}
+
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	//This is the list of Transform objects for the objectives
+	public List<Transform> objectives = new List<Transform>();
+
+	void Start () {
+	}
+	
+	void Update () {
+		
+	}
+
+	public void ExitGame () {
+		Application.Quit();
+	}
+}
