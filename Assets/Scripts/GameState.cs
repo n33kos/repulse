6fc9e19f853045e2edd9caee177c5ee20232c0fa
Timothy Williams <timtendow@gmail.com>
2017-01-27 22:24:51 -0700

    Added trash prefab and behavior. Added trash spawn function to the gamestate start.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,8 +6,10 @@ public class GameState : MonoBehaviour {
 
 	//This is the list of Transform objects for the objectives
 	public List<Transform> objectives = new List<Transform>();
+    public GameObject trash;
 
 	void Start () {
+        SpawnTrash();
 	}
 	
 	void Update () {
@@ -17,4 +19,16 @@ public class GameState : MonoBehaviour {
 	public void ExitGame () {
 		Application.Quit();
 	}
+
+    public void SpawnTrash () {
+        // Set first trash spawn position
+        float xPosition = -4.25f;
+        float xPositionOffset = 0.50f;
+        for (int i = 0; i < 16; i++) {
+            //Set correct spwan position
+            xPosition += xPositionOffset;
+            //Spawn a trash object
+            Instantiate(trash, new Vector3(xPosition,0f,0f), Quaternion.identity);
+        }
+    }
 }
---
+++ Assets/Scripts/TrashBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBehavior : MonoBehaviour {
+
+    public float xPos;
+    float yPos = 0f;
+
+
+    // Use this for initialization
+    void Start () {
+        setPosition(transform.position.x, yPos);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setPosition( float xPos, float yPos) {
+        transform.position = new Vector3(xPos, yPos, 0);
+    }
+}
